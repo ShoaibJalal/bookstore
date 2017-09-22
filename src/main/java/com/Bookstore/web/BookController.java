9d90b4470baaf603c30d3276942a35cb89915aa9
ShoaibJalal <shoaib.jalal@myy.haaga-helia.fi>
2017-09-22 15:17:09 +0300

    Adding edit Functionality

--- src/main/java/com/Bookstore/web/BookController.java
+++ src/main/java/com/Bookstore/web/BookController.java
@@ -18,12 +18,14 @@ public class BookController {
 	@Autowired
 	private CategoryRepository crepository;
 	
+	//Will Show all books in database
     @RequestMapping(value="/booklist")
     public String studentList(Model model) {	
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
-  
+    
+    //Will add new book to database
     @RequestMapping(value = "/add")
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
@@ -31,16 +33,25 @@ public class BookController {
         return "addbook";
     }     
     
+    //Save the book
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(Book book){
         repository.save(book);
         return "redirect:booklist";
     }    
-
+    
+    //Delete the book
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.delete(bookId);
         return "redirect:../booklist";
+    }
+    
+    @RequestMapping(value = "/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId,Model model){
+    	model.addAttribute("book", repository.findOne(bookId));
+    	model.addAttribute("categories",crepository.findAll());
+        return "editbook";
     }     
 
 }
